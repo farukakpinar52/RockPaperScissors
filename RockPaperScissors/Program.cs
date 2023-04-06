96232b63cdc9c3603ae1c22a8a7ab8ed3b7a226e
farukakpinar52 <faruk.akpinar@hotmail.com>
2023-04-07 02:42:27 +0300

    Kazanma/Kaybetme/Berabere kalma durumlarında renklendirmeler ile sonucu vurguladık. Ayrıca devam etme isteği sorulduğunda tek harf ile devam etme seçimini yapması için kullanıcıyı zorunlu kıldık.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -36,27 +36,46 @@ namespace RockPaperScissors
 
                 if (userChoice == computerChoice)
                 {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Berabere!");
+                    Console.ResetColor();
                 }
                 else if ((userChoice == 1 && computerChoice == 3) || (userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2))
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Kazandınız!");
+                    Console.ResetColor();
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Kaybettiniz!");
+                    Console.ResetColor();
                 }
 
-                Console.WriteLine("Devam etmek istiyor musunuz? (E/H)");
-                string continueChoice = Console.ReadLine();
-
-                if (continueChoice.ToLower() != "e")
+                while (true)
                 {
-                    break;
+                    Console.WriteLine("Devam etmek istiyor musunuz? (E/H)");
+                    string continueChoice = Console.ReadLine().ToLower();
+
+                    if (continueChoice == "e")
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else if (continueChoice == "h")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen 'E' veya 'H' harflerinden birini giriniz.");
+                    }
                 }
+
             }
         }
-
+        //yapılan seçimin değerini int parametresi olarak alıp onun metinsel karşılığını sunan metot
         static string GetChoiceName(int choice)
         {
             switch (choice)
